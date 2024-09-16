df93dceeeb43fd7553a4a887477447b2cb16cc95
GuavaMZ <guava.mzco@gmail.com>
2024-09-16 21:38:55 +0300

    hi

---
+++ lib/presentation/resources/routes_manager.dart
@@ -0,0 +1,14 @@
+import 'package:go_router/go_router.dart';
+
+class Routes {
+  static const String mainRoute = "/";
+  static const String loginRoute = "/login/view";
+  static const String profileRoute = "/profile/view";
+
+
+  final router = GoRouter(
+    routes: [
+
+    ]
+  );
+}
