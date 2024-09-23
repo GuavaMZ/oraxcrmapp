6936461d201333aea0ce09c126957fdfc8f48675
GuavaMZ <guava.mzco@gmail.com>
2024-09-23 18:20:25 +0300

    Hhhhhhhhh

--- lib/presentation/login/view/login_view.dart
+++ lib/presentation/login/view/login_view.dart
@@ -27,7 +27,7 @@ class _LoginViewState extends State<LoginView> {
             ),
             SvgPicture.asset('assets/images/tablet.svg'),
             SizedBox(
-              height: displayHeight(context) * 0.07,
+              height: displayHeight(context) * 0.04,
             ),
             Text(
               AppStrings.welcomeBack.getString(context),
@@ -41,16 +41,24 @@ class _LoginViewState extends State<LoginView> {
                   color: Color(0xffA1A1A1)),
             ),
             SizedBox(
-              height: displayHeight(context) * 0.07,
+              height: displayHeight(context) * 0.05,
             ),
             SizedBox(
-              width: 320,
+             width: displayWidth(context) * 0.9,
+                height: displayHeight(context) * 0.08,
               child: TextField(
                 obscureText: false,
                 decoration: InputDecoration(
+                  prefixIcon: Container(
+                    padding: const EdgeInsets.all(9.5),
+                    margin: const EdgeInsets.only(left: 8.0),
+                    child: SvgPicture.asset(
+                      'assets/images/phone.svg',
+                    ),
+                  ),
                   filled: true,
                   fillColor: const Color(0xffF5F4F4),
-                  labelText: AppStrings.phoneNumber.getString(context),
+                  hintText: AppStrings.phoneNumber.getString(context),
                   enabledBorder: const OutlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(30)),
                     borderSide: BorderSide(width: 0, color: Color(0xffffffff)),
@@ -69,24 +77,56 @@ class _LoginViewState extends State<LoginView> {
               ),
             ),
             SizedBox(
-              height: displayHeight(context) * 0.07,
+              height: displayHeight(context) * 0.050,
             ),
             SizedBox(
-              width: 320,
+             width: displayWidth(context) * 0.9,
+                height: displayHeight(context) * 0.08,
               child: TextField(
                 obscureText: true,
                 decoration: InputDecoration(
+                  prefixIcon: Container(
+                    padding: const EdgeInsets.all(9.5),
+                    margin: const EdgeInsets.only(left: 8.0),
+                    child: SvgPicture.asset(
+                      'assets/images/Lock.svg',
+                    ),
+                  ),
                   filled: true,
                   fillColor: const Color(0xffF5F4F4),
-                  labelText: AppStrings.password.getString(context),
-                  border: const OutlineInputBorder(
+                  hintText: AppStrings.password.getString(context),
+
+                  enabledBorder: const OutlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(30)),
+                    borderSide: BorderSide(width: 0, color: Color(0xffffffff)),
                   ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                    borderSide: BorderSide(width: 0, color: Color(0xffffffff)),
+                  ),
+                  // border: const OutlineInputBorder(
+                  //   borderRadius: BorderRadius.all(Radius.circular(30)),
+                  //   borderSide: BorderSide(width: 0, color: Color(0xffffffff)),
+                  // ),
                 ),
                 style: const TextStyle(
                     fontSize: 15, fontWeight: FontWeight.normal),
               ),
-            )
+            ),
+            SizedBox(
+              height: displayHeight(context) * 0.050,
+            ),
+            SizedBox(
+                width: displayWidth(context) * 0.9,
+                height: displayHeight(context) * 0.08,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(AppStrings.signIn.getString(context),
+                      style: TextStyle(color: ColorsManager.fontColor3)),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 0, 0, 0),
+                  ),
+                ))
           ]),
         ),
       ),
