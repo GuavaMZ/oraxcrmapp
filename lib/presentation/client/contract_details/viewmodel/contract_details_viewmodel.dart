daa8e21771dceffb90eab4687230b75a544138ff
GuavaMZ <guava.mzco@gmail.com>
2024-11-20 17:46:55 +0200

    zzzzz

--- lib/presentation/client/contract_details/viewmodel/contract_details_viewmodel.dart
+++ lib/presentation/client/contract_details/viewmodel/contract_details_viewmodel.dart
@@ -1,3 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:oraxcrm/data/api-request/contracts_requests.dart';
+import 'package:oraxcrm/presentation/resources/routes_manager.dart';
+import 'package:oraxcrm/presentation/resources/string_manager.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class ContractDetailsViewModel extends ChangeNotifier {}
+class ContractDetailsViewModel extends ChangeNotifier {
+  Map<String, String> signedStats = {
+    '0': AppStrings.unSigned,
+    '1': AppStrings.signed,
+  };
+
+  Future downloadContract(String id, String hash, BuildContext context) async {
+    ContractsRequests contractsRequests = ContractsRequests();
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    print(prefs.getString('usrToken'));
+
+    try {
+      await contractsRequests.downloadContract(
+          {'Authorization': prefs.getString('usrToken')},
+          id,
+          hash).then((res) async {
+        if (res.statusCode == 200) {
+        } else if (res.statusCode == 401) {
+          if (context.mounted) {
+            context.pushReplacement(Routes.loginRoute);
+          }
+        }
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+}
